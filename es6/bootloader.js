66e511358d0dbf09fd4c1d0f725cabee36f15d89
Rick Osborne <ricko@rickosborne.org>
2015-06-27 18:53:12 -0700

    Promises seem to work

--- es6/bootloader.js
+++ es6/bootloader.js
@@ -1,43 +1,100 @@
 export class BootLoader {
-  constructor() {
+  constructor(options) {
+    typeof options === 'object' || (options = {});
     this._resources = {};
+    this._promises = {};
   }
 
   resourceByName(name) {
     return this._resources[name];
   }
 
-  declareResource(definition) {
-    let resource = new Resource(definition);
+  promiseFor(name) {
+    let resource = this.resourceByName(name);
+    if (resource) return resource.promise;
+    if (this._promises[name]) return this._promises[name];
+    var resolver = null, rejecter = null, promise = new Promise(function (resolve, reject) {
+      resolver = resolve;
+      rejecter = reject;
+    });
+    promise.resolve = resolver;
+    promise.reject = rejecter;
+    return this._promises[name] = promise;
+  }
+
+  declareResource(resourceDefinition) {
+    this._addResource(new Resource(resourceDefinition));
+    return this;
+  }
+
+  _addResource(resource) {
+    if (!resource instanceof Resource) throw new InvalidResourceError();
     if (this._resources[resource.name]) throw new DuplicateResourceNameError(resource.name);
+    resource.promise = Promise.all(resource.requires.map((name) => this.promiseFor(name))).then((dependencies) => resource.resolveWith(dependencies));
+    if (this._promises[resource.name] && this._promises[resource.name].resolve) {
+      this._promises[resource.name].resolve(resource.promise);
+      delete this._promises[resource.name];
+    }
     this._resources[resource.name] = resource;
+    return this;
+  }
+
+  get undeclared() {
+    return Object.keys(this._promises);
   }
 
   [Symbol.iterator]() {
-
+    let resources = Object.keys(this._resources).sort().map((name) => this._resources[name]);
+    return {
+      next() {
+        return {
+          value: resources.shift(),
+          done: resources.length < 1
+        }
+      }
+    };
   }
 }
 
 class Resource {
   constructor(definition) {
     if (typeof definition !== 'object') throw new InvalidResourceError();
-    if (!definition.name) throw new UnnamedResourceError;
     this.name = definition.name;
+    if (!this.name) throw new UnnamedResourceError();
+    this.requires = definition.requires;
+    if (typeof this.requires === 'string') this.requires = [this.requires];
+    else if (!this.requires) this.requires = [];
+    if (!Array.isArray(this.requires)) throw new InvalidRequiresError();
+    this.provider = definition[this.name];
+    if (typeof this.provider !== 'function') throw new InvalidProviderError(this.name);
   }
+
+  resolveWith(dependencies) {
+    return this.provider(...dependencies);
+  }
+
+  //static buildMany(definition) {
+  //  if (typeof definition !== 'object') throw new InvalidResourceError();
+  //  if (definition.name) return [new Resource(definition)];
+  //  if (!definition.names || !Array.isArray(definition.names)) throw new UnnamedResourceError();
+  //  let resources = [];
+  //  for (let name of definition.names)
+  //    resources.push(new Resource(definition, name));
+  //  return resources;
+  //}
 }
 
 BootLoader.Resource = Resource;
 
-class DuplicateResourceNameError extends Error {
-  constructor(name) {
-    super();
-    this.message = `Duplicate resource name: ${name}`;
-  }
-}
+class DuplicateResourceNameError extends Error { constructor(name) { super(`Duplicate resource name: ${name}`); } }
 
-class UnnamedResourceError extends SyntaxError {}
 class InvalidResourceError extends Error {}
+class UnnamedResourceError extends SyntaxError {}
+class InvalidRequiresError extends SyntaxError {}
+class InvalidProviderError extends Error { constructor(name) { super(`Invalid provider: ${name}`); } }
 
 BootLoader.DuplicateReourceNameError = DuplicateResourceNameError;
 BootLoader.UnnamedResourceError = UnnamedResourceError;
+BootLoader.InvalidRequiresError = InvalidRequiresError;
 BootLoader.InvalidResourceError = InvalidResourceError;
+BootLoader.InvalidProviderError = InvalidProviderError;
--- spec/bootloader-spec.js
+++ spec/bootloader-spec.js
@@ -4,6 +4,14 @@ import {expect} from 'chai';
 describe('BootLoader', function() {
   it('is a class', () => expect(BootLoader).is.a('function'));
   describe('instance', () => {
+    let simpleResource = (name, req=null) => { return {name: name, [name]: () => 'simpleResourceResult', requires: req}; };
+    let simpleProvider = (function() {
+      var count = 0;
+      return (provider, req=null) => {
+        count++;
+        return {name: `test${count}`, test: provider, requires: req};
+      };
+    })();
     beforeEach(() => this.subject = new BootLoader());
     context('api', () => {
       let publicMethods = [
@@ -18,11 +26,69 @@ describe('BootLoader', function() {
     describe('declareResource', () => {
       it('requires a definition', () => expect(() => this.subject.declareResource()).throws(BootLoader.InvalidResourceError));
       it('requires a definition object', () => expect(() => this.subject.declareResource(1)).throws(BootLoader.InvalidResourceError));
-      it('requires a name', () => expect(() => this.subject.declareResource({})).throws(BootLoader.UnnamedResourceError));
-      it('requires a unique name', () => expect(() => {
-        this.subject.declareResource({name: 'same'});
-        this.subject.declareResource({name: 'same'});
+      it('requires a .name', () => expect(() => this.subject.declareResource({})).throws(BootLoader.UnnamedResourceError));
+      it('allows .requires to be a string', () => expect(this.subject.declareResource(simpleResource('test', 'req')).resourceByName('test').requires).to.eql(['req']));
+      it('requires .requires to be an array', () => expect(() => this.subject.declareResource(simpleResource('test', 1))).throws(BootLoader.InvalidRequiresError));
+      it('requires a unique .name', () => expect(() => {
+        this.subject.declareResource(simpleResource('one'));
+        this.subject.declareResource(simpleResource('one'));
       }).throws(BootLoader.DuplicateReourceNameError));
+      it('requires .provider to be a function', () => expect(() => this.subject.declareResource(simpleProvider(1))).throws(BootLoader.InvalidProviderError));
+    });
+    describe('resourceByName', () => {
+      it('returns defined resources', () => expect(this.subject.declareResource(simpleResource('test')).resourceByName('test')).to.be.an.instanceOf(BootLoader.Resource));
+      it('returns undefined when not found', () => expect(this.subject.resourceByName('test')).to.be.undefined);
+    });
+    describe('promiseFor', () => {
+      it('eventually resolves resources with no requirements', (done) => {
+        this.subject.declareResource(simpleResource('noReqs')).promiseFor('noReqs').then((resource) => {
+          expect(resource).to.eql('simpleResourceResult');
+          done();
+        });
+      });
+    });
+    describe('undeclared', () => {
+      it('starts empty', () => expect(this.subject.undeclared).to.be.empty);
+      it('reports undeclared requirements', () => expect(this.subject.declareResource(simpleResource('thing1', ['req1', 'req2'])).undeclared).to.contain('req1', 'req2'));
+      it('does not report declared requirements', (done) => {
+        this.subject.declareResource(simpleResource('thing1', ['req1','req2'])).declareResource(simpleResource('req1')).promiseFor('req1').then(() => {
+          expect(this.subject.undeclared).to.eql(['req2']);
+          done();
+        });
+      });
+    });
+    describe('iterator', () => {
+      it('starts empty', () => {
+        let resources = [];
+        for (let resource of this.subject) {
+          resources.push(resource);
+        }
+        expect(resources).to.be.empty;
+      });
+      it('includes only declared resources', () => {
+        this.subject.
+          declareResource(simpleResource('thing1', ['req1','req2'])).
+          declareResource(simpleResource('req1')).
+          declareResource(simpleResource('thing2', 'req1'))
+        ;
+        let resources = [];
+        for (let resource of this.subject) {
+          resources.push(resource.name);
+        }
+        expect(resources).to.contain('req1', 'thing1', 'thing2');
+        expect(resources).to.not.contain('req2');
+      });
+    });
+    describe('functionality', () => {
+      it('calls provider functions as their dependencies become available', (done) => {
+        let ops = [];
+        this.subject.
+          declareResource({name: 'A', requires: ['B', 'C'], A: () => ops.push('A')}).
+          declareResource({name: 'B', B: () => ops.push('B')}).
+          declareResource({name: 'C', requires: 'B', C: () => ops.push('C')})
+        ;
+        expect(ops).to.eql(['B', 'C', 'A']);
+      })
     });
   });
 });
